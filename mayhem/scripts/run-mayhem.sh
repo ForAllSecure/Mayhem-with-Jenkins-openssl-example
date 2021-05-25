830c45ab75bbc42c000df733017d973267a8e0ca
Sheldon Warkentin <sheldon.warkentin@gmail.com>
2021-05-25 13:22:30 -0600

    Remove regex matching from 'mayhem show' command

--- mayhem/scripts/run-mayhem.sh
+++ mayhem/scripts/run-mayhem.sh
@@ -153,7 +153,7 @@ if [[ "${BRANCH_NAME}" = "${PRIMARY_BRANCH}" ]]; then
 else
     # Find the latest run on the primary branch to determine the project/target
     # from which to download test cases.
-    LATEST_PRIMARY_TARGET=$(${CLI} show -n ${MAYHEM_ORGANIZATION} --format csv "^openssl/${MAYHEM_PRIMARY_TARGET}(?i)/\d+" | tail -n +2 | head -1 | cut -d"," -f1 | cut -d"/" -f"1,2"|| true)
+    LATEST_PRIMARY_TARGET=$(${CLI} show -n ${MAYHEM_ORGANIZATION} --format csv "openssl/${MAYHEM_PRIMARY_TARGET}" | tail -n +2 | head -1 | cut -d"," -f1 | cut -d"/" -f"1,2"|| true)
     if [[ -n "${LATEST_PRIMARY_TARGET}" ]]; then
 
         # Create a folder to download primary test suite into. This is to avoid
